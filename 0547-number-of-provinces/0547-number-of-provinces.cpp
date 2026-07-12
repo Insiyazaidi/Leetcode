a84574eb916ad7d3ff7c982cbb7a2a7d5f131680
Insiyazaidi <167618418+Insiyazaidi@users.noreply.github.com>
2026-07-13 02:37:47 +0530

    Time: 0 ms (100%), Space: 19.3 MB (92.82%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
 
-void dfs( vector<vector<int>>&adj  , vector<bool>&visited , int u){
+void dfs( vector<vector<int>>&isConnected , vector<bool>&visited , int u){
     if(visited[u]==true){
         return;
     }
     visited[u] = true;
-    for(auto &v:adj[u]){
-        if(!visited[v]){
-            dfs(adj , visited , v);
-        }
+for(int v = 0 ; v<isConnected.size(); v++){
+    if(isConnected[u][v]==1 && !visited[v]){ // direct vhi traverse krliya 
+        dfs(isConnected , visited , v);
     }
+}
+
+  //  for(auto &v:adj[u]){
+    //     if(!visited[v]){
+    //         dfs(adj , visited , v);
+    //     }
+    // }
 } 
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>>adj(n);
-        for(int i =0; i<isConnected.size(); i++){
-            for(int j = 0; j<isConnected.size() ; j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
+      //  vector<vector<int>>adj(n);
+        // for(int i =0; i<isConnected.size(); i++){
+        //     for(int j = 0; j<isConnected.size() ; j++){
+        //         if(i!=j && isConnected[i][j]==1){
+        //             adj[i].push_back(j);
                 
-                }
-            }
-        }
+        //         }
+        //     }
+        // }
+
 int count=0;
-vector<bool>visited(adj.size(),false);
-for(int i =0; i<adj.size(); i++){
+vector<bool>visited(n,false);
+for(int i =0; i<n; i++){
     if(!visited[i]){
-        dfs(adj , visited , i);
+        dfs(isConnected , visited , i);
         count++;
     }
 }
