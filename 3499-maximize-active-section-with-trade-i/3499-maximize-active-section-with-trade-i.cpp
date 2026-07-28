4eb8aff75fb3d37a5025d1a935c3de697df95489
Insiyazaidi <167618418+Insiyazaidi@users.noreply.github.com>
2026-07-28 19:00:56 +0530

    Time: 79 ms (69.18%), Space: 80.8 MB (49.51%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int ones = 0;
-        for (char c : s)
-            if (c == '1') ones++;
+        int activecount = count(begin(s) , end(s) , '1');
+        vector<int>zeroblocksize;
+        int n = s.size();
+       // int zerosize = 0;
+//         for(int i = 0 ; i<s.size() ; i++){
+//            if(s[i] == '0'){
+//             zerosize++;
+//            }
+//            else{  // agr 1 aagya 
+//    if(zerosize!=0){
+//     zeroblocksize.push_back(zerosize);
+//        zerosize = 0;
+//    }
 
-        string t = "1" + s + "1";
+//            }
+//         }
+int i = 0;
+while(i<n){
+if(s[i]=='0'){
+    int start = i;
+    while(i<n && s[i]=='0'){
+     i++;
+    }
+    zeroblocksize.push_back(i-start);
+}
+else{
+    i++;
+}
+}
 
-        vector<pair<char,int>> runs;
-
-        // Run-length encoding
-        for (char c : t) {
-            if (runs.empty() || runs.back().first != c)
-                runs.push_back({c, 1});
-            else
-                runs.back().second++;
+        int maxpairsum = 0;
+        for(int i =1 ; i<zeroblocksize.size() ; i++){
+            maxpairsum = max(maxpairsum , zeroblocksize[i]+zeroblocksize[i-1]);
         }
 
-        int ans = ones;
 
-        // Check every internal 1-block
-        for (int i = 1; i + 1 < runs.size(); i++) {
-            if (runs[i].first == '1' &&
-                runs[i - 1].first == '0' &&
-                runs[i + 1].first == '0') {
+return maxpairsum +activecount;
 
-                int gain = runs[i - 1].second + runs[i + 1].second;
-                ans = max(ans, ones + gain);
-            }
-        }
-
-        return ans;
     }
 };
