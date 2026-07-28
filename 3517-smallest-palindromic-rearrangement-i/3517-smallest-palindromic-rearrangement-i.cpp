361d91023031c5c6893b09fc15e480df0a1d5006
Insiyazaidi <167618418+Insiyazaidi@users.noreply.github.com>
2026-07-28 16:59:36 +0530

    Time: 123 ms (20.88%), Space: 70.2 MB (58.9%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,43 +1,17 @@
 class Solution {
 public:
-    string smallestPalindrome(string s) {
-        if(s.size()==1){
-            return s;
+    string smallestPalindrome(string s) { 
+        int n = s.size();
+      string left = s.substr(0 ,n/2 );
+      sort(left.begin() , left.end());
+      string ans = left;
+        if(n%2!=0){
+            ans += s[n/2] ; // middle element copied if present 
         }
-        map<char , int>mp;
-            int n = s.size();
-       string alpha(n , ' ');
+        reverse(left.begin() , left.end()); 
+        ans += left;
+        return ans;
 
-    int middle = n/2;
 
-// loop chlega n = 0 to n/2 -1 tk 
-for(int i = 0 ; i<middle ; i++){
-mp[s[i]]++;  // storing in map  
-}
-
-if(n%2!=0){  // odd length h 
-alpha[middle] = s[middle];
-}
- 
-
-    // map m store hogai value 
-int k  =0 ;
-   for(auto &it:mp){   // stored the char in ans 
-    while(it.second!=0){
-       alpha[k] = it.first;
-       k++;
-        it.second--;
-    }
-   }
-   int j = s.size()-1  ; // eg j = 5-1 = 4
-   int i = 0;
-  
-   while(i<j ){  // ans.size() = 3 , j - 4 , 3 
-      alpha[j] = alpha[i];
-      i++;
-      j--; 
-   }
-
-return alpha ;
     }
 };
