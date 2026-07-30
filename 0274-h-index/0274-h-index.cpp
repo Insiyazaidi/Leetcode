f315c5a1de103925c62ca3f4bffab134f9b07854
Insiyazaidi <167618418+Insiyazaidi@users.noreply.github.com>
2026-07-31 01:19:48 +0530

    Time: 0 ms (100%), Space: 12.1 MB (85.79%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-       int h =1;
-      sort( citations.begin() , citations.end());  // [0 ,1 , 3 , 5 , 6]
-       while(h<=citations.size()){
-    int highest = citations.size()-1 ;
-    int count =0;
-    while(highest>=0){
-        if(count>=h)  break;
-        if(citations[highest]>=h){
-            count++;
-        }
-        else if( citations[highest] < h && count<h) return h-1;
-        highest--;       
-    }
-h++;
-       } 
-       return  h-1 ;
+        sort(citations.begin(), citations.end());
+
+int n = citations.size();
+
+for(int i=0;i<n;i++)
+{
+    if(citations[i] >= n-i)
+        return n-i;
+}
+
+return 0;
     }
 };
