7aed46339a5b2f0be2cdae15ac432433c0969966
Insiyazaidi <167618418+Insiyazaidi@users.noreply.github.com>
2026-08-06 00:13:54 +0530

    Time: 350 ms (34.14%), Space: 344.8 MB (30.69%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-vector<int>ans; 
- vector<int> bfstraverse(int mainnode  ,vector<bool>&suspicious  , unordered_map<int , vector<int>>&adj , vector<bool>&visited ,  vector<vector<int>>& invocations , int n  ){
+
+ vector<int> bfstraverse(int mainnode  ,vector<bool>&suspicious  , unordered_map<int , vector<int>>&adj ,  int n  ){
 queue<int>q;
 q.push(mainnode);
-visited[mainnode] = true;
 suspicious[mainnode] = true;
 while(!q.empty()){
     int curr = q.front();
     q.pop();
     for(auto &v : adj[curr]){
-        if(!visited[v]){
+        if(!suspicious[v]){
             suspicious[v] = true;
-            visited[v] = true;
+          
           q.push(v); 
         }
     } 
 }
-
+vector<int>ans; 
 // nw check if non suspicious node is pointing out to suspicious or not ... 
-for(int i = 0 ; i<suspicious.size(); i++){
+for(int i = 0 ; i<n; i++){
     if(suspicious[i] == false){  // a non susp 
         for(auto &v : adj[i]){
             if(suspicious[v] == true){  // pointing to sus
@@ -35,7 +34,7 @@ return ans;
 
 // agr aisa nhi h toh ... non suspi vlo ko apne ans vec m daal do 
 
-for(int i = 0 ; i<suspicious.size() ; i++){
+for(int i = 0 ; i<n; i++){
     if(suspicious[i] == false){
         ans.push_back(i);
     }
@@ -45,13 +44,13 @@ return ans ;
 }
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
         
-// traversal dfs  or bfs .. 
+// traversal  bfs .. 
 // find the node that can be reached using k and mark them suspicious 
-// now if non suspicious node invoke suspicious removal is not posssible 
+// now if non suspicious node invoke suspicious removal is not posssible return all node from 0 to n 
 // if removal is possible -> then put non suspicious nodes in vector and return 
 
 vector<bool>suspicious(n , false);
-vector<bool>visited(n , false);
+
 unordered_map<int , vector<int>>adj;
 for(auto &e: invocations){
     int u = e[0];
@@ -59,7 +58,7 @@ for(auto &e: invocations){
 adj[u].push_back(v);
 }
 
- return  bfstraverse( k , suspicious, adj  , visited , invocations , n);
+ return  bfstraverse( k , suspicious, adj  ,  n);
 
 
 
