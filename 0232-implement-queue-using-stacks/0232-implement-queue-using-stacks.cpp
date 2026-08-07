add0c26b61884c6e287a218abac0d910d52ac0a7
Insiyazaidi <167618418+Insiyazaidi@users.noreply.github.com>
2026-08-07 15:08:03 +0530

    Time: 0 ms (100%), Space: 10 MB (99.11%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,63 @@
+class MyQueue {
+public:
+
+stack<int>input;
+stack<int>output;
+int peakele = 0; 
+
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(input.empty()){
+              peakele = x;  // agr yeh sbse phla element h 
+        }
+        input.push(x);
+    }
+    
+    int pop() {
+        if(!output.empty()){  // agr output empty nhi h toh yha se hi ans aaiga 
+            int storing = output.top();
+         output.pop();
+         return storing ;
+        }
+
+        // agr output empty h toh phir interchange kro 
+        while(!input.empty()){
+            output.push(input.top());
+            input.pop();
+        }
+        // ab saare elements output m  aagia h yha se ans aaiga 
+        int storing = output.top();
+         output.pop();
+         return storing ;
+    }
+    
+    int peek() {
+        if(output.empty()){   // output empty h toh hm yeh interchange krva process krskte the ... but hmne peek store 
+        // kiya hua h phle se toh no need 
+            return peakele; 
+        }
+        else{
+          return   output.top();// vrna output se ans niklalo 
+        }
+        
+    }
+    
+    bool empty() {  // dono stack empty h toh true 
+       if(input.empty() == true && output.empty() == true){
+        return true;
+       } 
+       return false;
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
